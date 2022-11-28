3f0cb2bacc9c5b679f4e6e611acd8216462ed7c9
angielkat <aksabogalbernal@gmail.com>
2022-11-27 21:19:31 -0500

    Implementación BackEnd
    Implementación cambios backend crear productos y consultar productos

--- tienda_virtual_back/app/models/products.js
+++ tienda_virtual_back/app/models/products.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
  
 const productSchema = new Schema({
-  talla: String,
-  color: String,
+  nombre: String,
+  descripcion: String,
   cantidad: Number,
   precio: Number,
   foto: String,
-  product_id: String, 
+  product_id: String
 });
  
 module.exports = mongoose.model("products", productSchema);
