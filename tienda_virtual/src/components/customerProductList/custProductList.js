6105358ca0730bf2259ef266a2e23bdc1760d052
sogarcia <sogarciamoreno@gmail.com>
2022-11-27 19:52:19 -0500

    Implementación de cambios frontEnd Crear Producto y Consultar productos

--- tienda_virtual/src/components/customerProductList/custProductList.js
+++ tienda_virtual/src/components/customerProductList/custProductList.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import '../../styles/Tabla.css';
 
 var heading = ['Id Producto', 'Nombre Producto','Descripcion Producto', 'Valor Unitario'];
